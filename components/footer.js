dc4c2e223cb04e89d60e5bb8bb7c53e0a199d444
itc-n23019 <n23019@std.it-college.ac.jp>
2024-01-25 06:40:36 +0000

    c

--- components/footer.js
+++ components/footer.js
@@ -5,12 +5,12 @@ import styles from 'styles/footer.module.css'
 const Footer = () => {
   return (
     <footer className={styles.wrapper}>
-<Container>
-	  <div className={styles.flexcontainer}>
-        <Logo />
-        [ソーシャル]
-      </div>
-	 </Container>
+      <Container>
+        <div className={styles.flexcontainer}>
+          <Logo />
+          [ソーシャル]
+        </div>
+      </Container>
     </footer>
   )
 }
