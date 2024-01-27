7c69721e741634e0c5611dc761d7d86d9294c550
itc-n23019 <n23019@std.it-college.ac.jp>
2024-01-27 15:07:49 +0000

    crear chap5

--- components/footer.js
+++ components/footer.js
@@ -1,16 +1,17 @@
 import Container from 'components/container'
 import Logo from 'components/logo'
 import styles from 'styles/footer.module.css'
-
+import Social from 'components/social'
 const Footer = () => {
   return (
     <footer className={styles.wrapper}>
-      <Container>
-        <div className={styles.flexContainer}>
-          <Logo />
-          [ソーシャル]
-        </div>
-      </Container>
+<Container>
+	  <div className={styles.flexContainer}>
+        <Logo />
+
+	  <Social />
+      </div>
+	 </Container>
     </footer>
   )
 }
