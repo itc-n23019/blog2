53d06107467a056b67f827697219c08f3016062c
itc-n23019 <n23019@std.it-college.ac.jp>
2024-01-31 14:12:17 +0000

    crewa chap8

--- components/two-column.js
+++ components/two-column.js
@@ -1,14 +1,14 @@
 import styles from 'styles/two-column.module.css'
 
-const Twocolumn = ({ children }) => {
+const TwoColumn = ({ children }) => {
   return <div className={styles.flexContainer}>{children}</div>
 }
 
-const TwocolumnMain = ({ children }) => {
+const TwoColumnMain = ({ children }) => {
   return <div className={styles.main}>{children}</div>
 }
 
-const TwocolumnSidebar = ({ children }) => {
+const TwoColumnSidebar = ({ children }) => {
   return <div className={styles.sidebar}>{children}</div>
 }
-export { Twocolumn, TwocolumnMain, TwocolumnSidebar }
+export { TwoColumn, TwoColumnMain, TwoColumnSidebar }
