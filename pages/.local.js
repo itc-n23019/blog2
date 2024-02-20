c9200cdb29f30cd90dd66f2ace64ba8078613f03
itc-n23019 <n23019@std.it-college.ac.jp>
2024-02-20 06:18:02 +0000

    build err vercel crea

--- pages/.local.js
+++
@@ -1,7 +0,0 @@
-const Counter: FC = () => {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount((count) => count + 1);
-
-  return <button onClick={increment}>{count}</button>;
-};
-
